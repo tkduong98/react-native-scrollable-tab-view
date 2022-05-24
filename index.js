492de183a81503ba81508e4999076ed3bdac65ed
tkduong98 <55155107+tkduong98@users.noreply.github.com>
2022-05-24 11:16:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {
 
 const ViewPagerAndroid = require('@react-native-community/viewpager');
 const TimerMixin = require('react-timer-mixin');
-const ViewPager = require('@react-native-community/viewpager');
+const ViewPager = require('react-native-pager-view');
 
 const SceneComponent = require('./SceneComponent');
 const DefaultTabBar = require('./DefaultTabBar');
